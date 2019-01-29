42b1f76a08c23a68bff4dbd6f27e53f013e98d7f
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-28 19:00:02 -0500

    created router configuration

--- lib/engine_web/router.ex
+++ lib/engine_web/router.ex
@@ -1,26 +1,15 @@
 defmodule EngineWeb.Router do
   use EngineWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", EngineWeb do
-    pipe_through :browser
+  scope "/api" do
+    pipe_through :api
 
-    get "/", PageController, :index
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: EngineWeb.Schema
+
+    forward "/", Absinthe.Plug, schema: EngineWeb.Schema
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", EngineWeb do
-  #   pipe_through :api
-  # end
 end
